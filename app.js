cfe19337f576eb4fcd920ea68ad485392222f850
Jargalan <tamirjargalan2@gmail.com>
2021-06-04 14:27:19 +0800

    Toglogchiin eeljiig solidog bolgoh

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, 2-р тоглогчийг 1 гэж тэмдэглэе.
-var activePlayer = 1;
+var activePlayer = 0;
 
 // Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
@@ -21,11 +21,40 @@ document.getElementById("current-0").textContent = "0";
 document.getElementById("current-1").textContent = "0";
 var diceDom = document.querySelector(".dice");
 diceDom.style.display = "none";
+
+//Шоог шидэх эвент листенер
+
 document
   .querySelector(".btn-roll")
   .addEventListener("click", function shooShid() {
+    // 1-6 доторх санамсаргүй нэг тоо гаргаж авна.
     var diceNumber = Math.floor(Math.random() * 6) + 1;
-
+    //Шооны зургийг веб дээр гаргаж ирэх
     diceDom.style.display = "block";
+    // Буусан санамсаргүй тоонд харгалзах шооны зургийг веб дээр гаргаж ирнэ.
     diceDom.src = "dice-" + diceNumber + ".png";
+
+    // Буусан тоо нь 1-ээс ялгаатай бол идэвхтэй тоглогчийн ээлжийн оноог нэмэгдүүлнэ.
+    if (diceNumber !== 1) {
+      //1-ээс ялгаатай тоо буулаа. Буусан тоог тоглогчид нэмж өгнө.
+      roundScore = roundScore + diceNumber;
+      document.getElementById("current-" + activePlayer).textContent =
+        roundScore;
+    } else {
+      //1 буусан тул тоглогчийн ээлжийг солино.
+
+      //Энэ тоглогчийн ээлжиндээ цуглуулсан оноог 0 болгоно.
+      roundScore = 0;
+      document.getElementById("current-" + activePlayer).textContent = 0;
+      //Тоглогчийн ээлжийг нөгөө тоглогч руу шилжүүлнэ.
+      //Хэрэв идэвхтэй тоглогч 0 байвал идэвхтэй тоглогчийг 1 болго. Үгүй бол идэвхтэй тоглогчийг 0 болго.
+      activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+      //Улаан цэгийг шилжүүлэх код
+      document.querySelector(".player-0-panel").classList.toggle("active");
+      document.querySelector(".player-1-panel").classList.toggle("active");
+
+      //Солигдох үед шоог түр алга болгох
+      diceDom.style.display = "none";
+    }
   });
